6e56150fb707cb31ed0197e3adff4c4cc71d8bc5
Bryce Borba <borbaesb@gmail.com>
2022-03-25 09:41:06 -0700

    menu list is now ordered by menuId

--- assets/js/menu/FoodCard.js
+++ assets/js/menu/FoodCard.js
@@ -1,19 +1,24 @@
 export const FoodCard = (foodObj) => {
-
     return `
-    <div class="col-sm-6 g-4">
-        <div class="card">
-        <div class="h-100">
-        <img src="./assets/images/${foodObj.imageURL}" class="card-img-top" alt="${foodObj.imageURL}">
-        </div>
-            <div class="card-body">
-                <h5 class="card-title">${foodObj.name}</h5>
-                <p class="card-text">${foodObj.description}</p>
-                <p>Price: $${foodObj.price}</p>
-                <!-- <button type="button" class="btn btn-primary">Example Bootstrap Button</button> -->
-            </div>
-        </div>
-    </div>
-    `
-}
-
+      <div class="col-sm-6 g-4">
+          <div class="card">
+          <div class="h-100">
+          <img src="./assets/images/${
+            foodObj.imageURL
+          }" class="card-img-top" alt="${foodObj.imageURL}">
+          </div>
+              <div class="card-body">
+                  <h5 class="card-title">${foodObj.name}</h5>
+                  <p class="card-text">${foodObj.description}</p>
+                  <p>Price: $${foodObj.price}</p>
+                  <p>${
+                    foodObj.isVegetarian
+                      ? `<img src ="./assets/images/icon-veg.png" alt="IsVegetarian">`
+                      : ""
+                  }</p>
+                  <!-- <button type="button" class="btn btn-primary">Example Bootstrap Button</button> -->
+              </div>
+          </div>
+      </div>
+      `;
+  };
--- assets/js/menu/FoodsList.js
+++ assets/js/menu/FoodsList.js
@@ -2,14 +2,49 @@ import * as MenuManger from './MenuManger.js';
 import { FoodCard } from './FoodCard.js'
 
 export const FoodList = () => {
-    const contentTarget = document.querySelector("main")
-    let HTMLString;
+    const contentTarget = document.querySelector("main");
+    let lunchHTMLString;
+    let drinkHTMLString;
+    let appHTMLString;
+    let dessertHTMLString;
     MenuManger.getFoods()
     .then(foodArray => {
-        HTMLString = `<div class="row">`
-        HTMLString += foodArray.map(food => FoodCard(food)).join('')
-        HTMLString += `</div>`
-        contentTarget.innerHTML = HTMLString;
+        let lunchArray=[];
+        let dessertArray = [];
+        let drinkArray = [];
+        let appArray = [];
+        foodArray.map(food => {
+            if(food.menuId === 1){
+                lunchArray.push(food)
+        } else if(food.menuId === 2){
+            drinkArray.push(food)
+        }else if(food.menuId === 3){
+            appArray.push(food)
+        }else if(food.menuId === 4){
+            dessertArray.push(food)
+        }
     })
-    
+
+        lunchHTMLString = `<h2>Lunch</h2>`
+        lunchHTMLString += `<div class="row lunch">`
+        lunchHTMLString += lunchArray.map(food => FoodCard(food)).join('')
+        lunchHTMLString += `</div>`
+
+        drinkHTMLString = `<h2>Drinks</h2>`
+        drinkHTMLString += `<div class="row drink">`
+        drinkHTMLString += drinkArray.map(food => FoodCard(food)).join('')
+        drinkHTMLString += `</div>`
+
+        appHTMLString = `<h2>Appetizers</h2>`
+        appHTMLString += `<div class="row appetizer">`
+        appHTMLString += appArray.map(food => FoodCard(food)).join('')
+        appHTMLString += `</div>`
+
+        dessertHTMLString = `<h2>Desserts</h2>`
+        dessertHTMLString += `<div class="row dessert">`
+        dessertHTMLString += dessertArray.map(food => FoodCard(food)).join('')
+        dessertHTMLString += `</div>`
+
+        contentTarget.innerHTML = lunchHTMLString + drinkHTMLString + appHTMLString + dessertHTMLString
+    })
 }
