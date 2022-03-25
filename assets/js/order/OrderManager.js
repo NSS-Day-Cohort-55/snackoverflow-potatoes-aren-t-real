dab941262d27a7e55bd90d9be9b71243fc7102e2
LeeRJennings <leerjennings@outlook.com>
2022-03-25 14:07:32 -0500

    added functions for posting and deleting orders and orderFoods

--- assets/js/order/OrderManager.js
+++ assets/js/order/OrderManager.js
@@ -9,3 +9,49 @@ export const getOrderFoods = () => {
     return fetch(`${apiURL}/orderFoods`)
     .then(response => response.json())
 }
+
+export const postOrder = (orderObj) => {
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(orderObj)
+    }
+  
+    return fetch(`http://localhost:8088/orders`, fetchOptions);
+  }
+
+  export const postOrderFood = (orderFoodObj) => {
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(orderFoodObj)
+    }
+  
+    return fetch(`http://localhost:8088/orderFoods`, fetchOptions);
+  }
+
+export const deleteOrder = (orderObj) => {
+    const fetchOptions = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    }
+
+    return fetch(`http://localhost:8088/orders`, fetchOptions)
+}
+
+export const deleteOrderFood = (orderFoodObj) => {
+    const fetchOptions = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    }
+
+    return fetch(`http://localhost:8088/orderFoods`, fetchOptions)
+}
